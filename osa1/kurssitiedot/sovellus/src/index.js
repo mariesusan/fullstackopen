b8120ea22dfa1413f8ae37448c9773a536a4fea9
Marie Puhakka <mariesusanpuhakka@gmail.com>
2021-01-28 14:12:46 +0200

    1.2: kurssitiedot, step2

--- osa1/kurssitiedot/sovellus/src/index.js
+++ osa1/kurssitiedot/sovellus/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    // const maarittelyt
     const course = "Half Stack application development";
     const part1 = "Fundamentals of React";
     const exercises1 = 10;
@@ -10,18 +11,12 @@ const App = () => {
     const part3 = "State of a component";
     const exercises3 = 14;
 
-    // const maarittelyt
-
     return (
         <>
             <Header course={course} />
             <br/>
-            <Content part={part1} />
-            <br/>
-            <Content part={part2} />
-            <br/>
-            <Content part={part3} />
-            <br/>
+            <Content part1={part1} part2={part2} part3={part3}
+            sumOf1={exercises1} sumOf2={exercises2} sumOf3={exercises3} />
             <Total sum={exercises1 + exercises2 + exercises3} />
             <br/>
         </>
@@ -39,7 +34,17 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            {props.part}
+            <Part partOf={props.part1} totalOf={props.sumOf1} />
+            <Part partOf={props.part2} totalOf={props.sumOf2} />
+            <Part partOf={props.part3} totalOf={props.sumOf3} />
+        </>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <>
+            {props.partOf} {props.totalOf} <br/>
         </>
     )
 }
