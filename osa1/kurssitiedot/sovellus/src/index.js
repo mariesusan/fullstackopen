9a743fa718e1afb511b301a22861d5476f9e4be9
Marie Puhakka <mariesusanpuhakka@gmail.com>
2021-01-29 13:22:51 +0200

    1.3: kurssitiedot, step3

--- osa1/kurssitiedot/sovellus/src/index.js
+++ osa1/kurssitiedot/sovellus/src/index.js
@@ -4,20 +4,28 @@ import ReactDOM from 'react-dom';
 const App = () => {
     // const maarittelyt
     const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
-    const exercises1 = 10;
-    const part2 = "Using props to pass data";
-    const exercises2 = 7;
-    const part3 = "State of a component";
-    const exercises3 = 14;
+    const part1 = {
+        name: "Fundamentals of React",
+        exercises: 10
+    }
+
+    const part2 = {
+        name: "Using props to pass data",
+        exercises: 7
+    }
+
+    const part3 = {
+        name: "State of a component",
+        exercises: 14
+    }
 
     return (
         <>
             <Header course={course} />
             <br/>
-            <Content part1={part1} part2={part2} part3={part3}
-            sumOf1={exercises1} sumOf2={exercises2} sumOf3={exercises3} />
-            <Total sum={exercises1 + exercises2 + exercises3} />
+            <Content part1={part1.name} part2={part2.name} part3={part3.name}
+            sumOf1={part1.exercises} sumOf2={part2.exercises} sumOf3={part3.exercises} />
+            <Total sum={part1.exercises + part2.exercises + part3.exercises} />
             <br/>
         </>
     )
